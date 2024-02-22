a66fdfbc03866835de3316392f3ac7e3d64fd730
TechnoChiken379 <luke.vd.tillaar@gmail.com>
2024-02-22 13:47:22 +0100

    push

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ document.getElementById("mainTitle").innerText = "PointAndClickAdventureGame";
 //Game window reference
 const gameWindow = document.getElementById("gameWindow");
 
+//Inventory
+const inventoryWindow = document.getElementById("inventoryWindow");
+
 //Main character
 const mainCharacter = document.getElementById("hero");
 const offsetCharacter = 16;
@@ -14,15 +17,29 @@ gameWindow.onclick = function (e) {
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
 
-    mainCharacter.style.left = x - offsetCharacter + "px";
-    mainCharacter.style.top = y - offsetCharacter + "px";
+    if(e.target.id !== "heroImage")
+    {
+        mainCharacter.style.left = x - offsetCharacter + "px";
+        mainCharacter.style.top = y - offsetCharacter + "px";
+    }
+    
     switch (e.target.id) {
         case "squareTree":
             tree1.style.opacity = 0.2;
         case "key":
             console.log("YOU FOUND KEY");
+            document.getElementById("key").remove;
+            const KeyElement = document.createElement("key").remove();
+            KeyElement.id = "inv-key";
+            KeyElement.innerText = "key";
+            inventoryList.appendChild.KeyElement;
             break;
         default:
             tree1.style.opacity = 1;
+    }
+
+    function getItem(itemName, itemId)
+    {
+
     }
 }
